c94da94cbca6ba99afc435eb55bd89ee2c8c39fb
shelodavid <sheldon.o.david@gmail.com>
2026-02-01 18:54:06 -0500

    Fix ETL worker lifetimes and run modes

--- Etl/Options/EtlExecutionOptions.cs
+++ Etl/Options/EtlExecutionOptions.cs
@@ -4,6 +4,7 @@ public class EtlExecutionOptions
 {
     public bool RunOnce { get; set; }
     public bool QueueOnly { get; set; }
+    public bool QueueRunOnce { get; set; }
     public string? SourceFilter { get; set; }
     public DateTimeOffset? Since { get; set; }
     public int PageSize { get; set; } = 50;
