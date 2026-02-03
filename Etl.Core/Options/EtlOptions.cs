2a84f2eed53101b8f63ef8be45a23c386338f5da
shelodavid <sheldon.o.david@gmail.com>
2026-02-02 21:03:12 -0500

    Add fixture options to ETL settings

--- Etl.Core/Options/EtlOptions.cs
+++ Etl.Core/Options/EtlOptions.cs
@@ -3,6 +3,8 @@ namespace RentWisePro.Etl.Core.Options;
 public class EtlOptions
 {
     public bool DevMode { get; set; }
+    public bool UseFixtures { get; set; }
+    public string? FixtureRootPath { get; set; }
     public int MarkOffMarketAfterMissingRuns { get; set; } = 3;
     public int MaxPhotosPerProperty { get; set; } = 10;
     public int PhotoDownloadConcurrency { get; set; } = 4;
